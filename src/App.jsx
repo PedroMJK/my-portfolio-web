93d9a8b3e7d71ef873e3cf6a2d77f82f8c5e74f7
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-11-26 15:14:21 -0300

    update background color

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
   return (
     <div className='overflow-x-hidden text-stone-300 antialiased'>
       <div className='fixed inset-0 -z-10'>
-        <div className='relative h-full w-full bg-black'>
-          <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linea-gradient(to_right,#4f4f4f2e_1px,transparent_1px,
+      <div className='relative h-full w-full bg-gradient-to-b from-emerald-900 to-black'>
+      <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linea-gradient(to_right,#4f4f4f2e_1px,transparent_1px,
           linear-gradient_bottom,#8080800a_1px,tranparent_1px),
           bg-[size:14px_24px]'></div>
           <div className='absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(cicle_400px_at_50%_300px,
